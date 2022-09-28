19288e4ae258e4ee5577244cb5cdd2e9bd2e5b49
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 12:00:00 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "6693e704-2729-4c9c-84b7-2d2a16066f70",
+    "home_feed": [
+        {
+            "id": "528049db-ff24-45aa-b852-7c449e72907d",
+            "artist_name": "executive",
+            "home_carousel": [
+                {
+                    "id": "30198d24-4211-4e3e-ac7c-60f4890ed697",
+                    "art_photo": "https://images.unsplash.com/source-404?w=1080"
+                },
+                {
+                    "id": "0c32143d-bc00-464b-9ef6-6efbcd0efd0f",
+                    "art_photo": "https://images.unsplash.com/photo-1634733988138-bf2c3a2a13fa?w=1080"
+                },
+                {
+                    "id": "aedda229-d534-419a-b61c-fee72ed75d71",
+                    "art_photo": "https://images.unsplash.com/photo-1518534249708-e8f3537753ee?w=1080"
+                },
+                {
+                    "id": "82f73fa1-9b5b-4aeb-ad7b-ca47ca583c1d",
+                    "art_photo": "https://images.unsplash.com/photo-1606632730495-a40b1c9adcb4?w=1080"
+                },
+                {
+                    "id": "d94c0431-ec99-48d7-a48e-5a0381385e06",
+                    "art_photo": "https://images.unsplash.com/photo-1612197464930-22478fa5fc1d?w=1080"
+                }
+            ],
+            "art_caption": "under",
+            "art_description": "Return leader first food soldier. Thing sell today fact order. Son past still somebody mission debate member."
+        },
+        {
+            "id": "dd158806-eb26-4157-ba1a-17ccb8f44c89",
+            "artist_name": "whose",
+            "home_carousel": [
+                {
+                    "id": "3fb4a78e-2426-4f88-b1a9-15dacd7ddab0",
+                    "art_photo": "https://images.unsplash.com/photo-1587714656374-fb3c9c3a4977?w=1080"
+                },
+                {
+                    "id": "403ce975-e5e1-4c60-adac-f3f47826d302",
+                    "art_photo": "https://images.unsplash.com/photo-1624483274779-2a4224fcd30a?w=1080"
+                },
+                {
+                    "id": "fd60c00c-668b-4204-ac50-58817a157831",
+                    "art_photo": "https://images.unsplash.com/photo-1562516155-e0c1ee44059b?w=1080"
+                },
+                {
+                    "id": "4edbc91b-f675-487e-bf20-e3f8908d3a48",
+                    "art_photo": "https://images.unsplash.com/photo-1647025475692-677cf67c5608?w=1080"
+                },
+                {
+                    "id": "04062ee3-d756-48ec-b894-3bd65307893c",
+                    "art_photo": "https://images.unsplash.com/photo-1642506539692-99e21cbd2f5c?w=1080"
+                }
+            ],
+            "art_caption": "include",
+            "art_description": "Resource point million. Issue care against cultural beautiful of."
+        },
+        {
+            "id": "9ea7b1e1-4063-4d65-a8c1-b4f2a73aa27f",
+            "artist_name": "heart",
+            "home_carousel": [
+                {
+                    "id": "caea4918-231f-48bf-8811-a6cb59cc838d",
+                    "art_photo": "https://images.unsplash.com/photo-1622037738094-fab81aee9fd3?w=1080"
+                },
+                {
+                    "id": "957d2c21-2868-4988-ad18-1ba22fcb1114",
+                    "art_photo": "https://images.unsplash.com/photo-1640364596762-e5b1d0966a5f?w=1080"
+                },
+                {
+                    "id": "b296cc53-7fdb-4939-afa3-880aa07e1d3e",
+                    "art_photo": "https://images.unsplash.com/photo-1637511916663-93dd81076890?w=1080"
+                },
+                {
+                    "id": "2ec0d618-36c3-4fe3-bba2-240f25bbd167",
+                    "art_photo": "https://images.unsplash.com/photo-1612185079092-abf5793ec85c?w=1080"
+                },
+                {
+                    "id": "43951389-f96a-4d5f-8a74-fac5d0a5707c",
+                    "art_photo": "https://images.unsplash.com/photo-1625806335823-d967b0099d2b?w=1080"
+                }
+            ],
+            "art_caption": "energy",
+            "art_description": "Across would yourself. Court about according attention half trip. Watch personal deal chair."
+        },
+        {
+            "id": "042dc5cc-af2d-4005-8941-c3f52fe7acea",
+            "artist_name": "cut",
+            "home_carousel": [
+                {
+                    "id": "0ed21afa-00d8-4400-8b5e-c644489695b0",
+                    "art_photo": "https://images.unsplash.com/photo-1518960424672-59c7cd51f4ba?w=1080"
+                },
+                {
+                    "id": "c24f61ab-d180-4491-8260-835f02af1804",
+                    "art_photo": "https://images.unsplash.com/photo-1588018760999-a58cbf36658e?w=1080"
+                },
+                {
+                    "id": "ff0810cd-b9f6-4dbc-8be4-4535776988aa",
+                    "art_photo": "https://images.unsplash.com/photo-1434715961933-4cbf3d79230f?w=1080"
+                },
+                {
+                    "id": "bc9ca6b7-7fea-42b3-8d10-8e8f0043f9f8",
+                    "art_photo": "https://images.unsplash.com/photo-1587813369210-4ce93b19bcfe?w=1080"
+                },
+                {
+                    "id": "b266c9f3-3817-41ff-8444-cdfe9001831d",
+                    "art_photo": "https://images.unsplash.com/photo-1629783509182-68c8c190e952?w=1080"
+                }
+            ],
+            "art_caption": "need",
+            "art_description": "Animal voice result run front seek. Minute thought nothing region Congress anyone. Ready benefit civil big. Effect two black unit like."
+        },
+        {
+            "id": "3bb76e9e-b7f8-4fa1-b31e-f33be42d12ce",
+            "artist_name": "oil",
+            "home_carousel": [
+                {
+                    "id": "a39976b5-986b-4972-8b15-6e6da63da03f",
+                    "art_photo": "https://images.unsplash.com/photo-1525268771113-32d9e9021a97?w=1080"
+                },
+                {
+                    "id": "680fcb4e-1230-4cdb-809a-fbf2f3811546",
+                    "art_photo": "https://images.unsplash.com/photo-1531040630173-7cfb894c8eaa?w=1080"
+                },
+                {
+                    "id": "16c7aaf8-d617-4f8b-bf9f-2bbcd7f14f51",
+                    "art_photo": "https://images.unsplash.com/photo-1639024473524-66c65669cdb1?w=1080"
+                },
+                {
+                    "id": "0ea9425f-58a7-471e-9eb8-6a5792e1bdb5",
+                    "art_photo": "https://images.unsplash.com/photo-1573879410138-96c2a1ad3210?w=1080"
+                },
+                {
+                    "id": "ad7ceab5-557a-4878-83e9-57e8e743e246",
+                    "art_photo": "https://images.unsplash.com/photo-1595820920927-075e2eec2abe?w=1080"
+                }
+            ],
+            "art_caption": "time",
+            "art_description": "Court return drive very already always produce. Everybody degree hand another. Behind of perform career hope."
+        }
+    ]
+}
